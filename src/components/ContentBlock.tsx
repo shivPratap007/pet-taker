d9bc47bbeab57961204def91eb687c9e8861f61e
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-07-06 19:29:51 +0530

    Add: pet list component

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -5,5 +5,5 @@ export default function ContentBlock({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-white shadow-lg rounded-sm text-white py-8 overflow-hidden h-full w-full">{children}</div>;
+  return <div className="bg-white shadow-lg rounded-sm text-white  overflow-hidden h-full w-full">{children}</div>;
 }
--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function PetList() {
   return (
-    <ul>
-        <li className='bg-white border-b'>
-            <button>
-                Dobby
-            </button>
-        </li>
+    <ul className="bg-white border-b border-black/[.08]">
+      <li className="bg-white text-black border-b p-3 ">
+        <button className="items-center gap-2 flex h-[70ps] w-full cursor-pointer hover:bg-[EFF1F2] focus:bg-[EFF1F2]">
+          <Image
+            className="rounded-full object-cover"
+            src={
+              "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            }
+            alt="Pet image"
+            width={45}
+            height={45}
+          />
+          <p className="font-semibold">Dobby</p>
+        </button>
+      </li>
     </ul>
-  )
+  );
 }
